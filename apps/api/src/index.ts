c22cb6e1b7541653cd487d27342c3e55a675834a
Israel R. Jatobá <yeytaken@outlook.com>
2025-12-05 17:49:30 -0300

    feat: add api system

---
+++ apps/api/src/index.ts
@@ -0,0 +1,15 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('ARC Studio, api.')
+})
+
+serve({
+  fetch: app.fetch,
+  port: 4000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
